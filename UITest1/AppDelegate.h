6883ff15c5144f32736d74d5f92613624ac9f97b
Igor Mozhay <mozhay@igors-imac.local>
2016-06-09 19:50:48 +0600

    Initial Commit

---
+++ UITest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITest1
+//
+//  Created by Igor Mozhay on 09/06/16.
+//  Copyright © 2016 Igor Mozhay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITest1
+//
+//  Created by Igor Mozhay on 09/06/16.
+//  Copyright © 2016 Igor Mozhay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
